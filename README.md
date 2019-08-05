3336e969c5cd014fd7d42508c6b88a80b36aa6f8
Mahmoud Hamdy <Mahmou-El-Deeb@hotmail.com>
2019-08-06 00:03:49 +0200

    V1.00.01 First edition of MemoGame! This is an amazing thing to me ⊙ｏ⊙）

---
+++ README.md
@@ -0,0 +1,21 @@
+# MeMo Game `v1.00.01`
+---
+# ***```My First Game```***
+**Languages Support**
+1. HTML
+2. CSS
+3. JS
+**Framework Used**
+1. Bootstrap
+2. JQuery
+3. Font Awesome
+## How To Use
+- ***FirstTime:*** `Go straight to game!`
+- ***YouLose ?!*** `No problem just hit tryAgain`
+
+ 
+# `Next version info v1.00.04`
+---
+1. *Create win condition.*
+2. *UI improvement.*
+3. *Put brief about game.*
--- js/script.js
+++ js/script.js
@@ -1,3 +1,60 @@
-$(document).ready(function() {
+images = document.getElementsByClassName('image');
+imagesLength = document.querySelectorAll('.image').length;
+for (let i = 0; i < imagesLength; i++) {
+        images[i].addEventListener('click', function () {
+                if (images[i].classList[1] != 'success') {
+                        checkCard = document.getElementsByClassName('checkCard').length;
+                        if (checkCard < 2) {
+                                images[i].classList.toggle('checkCard');
+                        }
+                        if (checkCard == 1) {
+                                imageMatch = document.getElementsByClassName('checkCard')
+                                if (imageMatch[0].getAttribute('code') == imageMatch[1].getAttribute('code')) {
+                                        imageMatch[0].classList.add('success');
+                                        imageMatch[1].classList.add('success');
+                                        imageMatch[1].classList.remove('checkCard');
+                                        imageMatch[0].classList.remove('checkCard');
+                                } else {
+                                        if (document.getElementsByClassName('fa-heart').length > 0) {
+                                                document.getElementsByClassName('fa-heart')[0].remove()
+                                        }
+                                        if (document.getElementsByClassName('fa-heart').length == 1) {
+                                                document.querySelector('.fa-heart').classList.add('lastLife')
+                                        }
+                                        imageMatch[0].classList.add('fail');
+                                        imageMatch[1].classList.add('fail');
+                                        imageMatch[1].classList.remove('checkCard');
+                                        imageMatch[0].classList.remove('checkCard');
+                                        if (document.getElementsByClassName('fa-heart').length == 0) {
+                                                document.querySelector('#gameOver').classList.remove('none');
+                                                document.querySelector('.gameImages').classList.add('gameOver');
+                                        }
+                                }
+                                if (document.getElementsByClassName('fail').length > 0) {
+                                        imageNotMatch = document.getElementsByClassName('fail');
+                                        setTimeout(function () {
+                                                imageNotMatch[0].classList.remove('fail');
+                                                imageNotMatch[0].classList.remove('fail');
+                                        }, 1500)
+                                }
+                        }
+                }
+                if (document.getElementsByClassName('success').length == 16) {
+                        // Win The MemoGame
+                }
+        })
+}
+const tryAgain = document.getElementById('tryAgain');
+tryAgain.addEventListener('click', function () {
+        let gameOver = document.getElementById('gameOver');
+        let gameOverImages = document.querySelector('.gameImages');
+        gameOver.classList.add('none');
+        gameOverImages.classList.remove('gameOver');
+        for (i = 0; i < 3; i++) {
+                let lifeReset = document.querySelector('.life');
+                let lifeHeart = document.createElement('span')
+                lifeHeart.classList.add('fas', 'fa-heart');
+                lifeReset.appendChild(lifeHeart)
+        }
 
-});
+})
